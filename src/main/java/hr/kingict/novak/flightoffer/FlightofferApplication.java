d7fa97bebd12b523e01f50adba00d1780561163f
tompa402 <tomislavno@gmail.com>
2019-04-10 20:26:28 +0200

    Initial commit

---
+++ src/main/java/hr/kingict/novak/flightoffer/FlightofferApplication.java
@@ -0,0 +1,13 @@
+package hr.kingict.novak.flightoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightofferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightofferApplication.class, args);
+    }
+
+}
